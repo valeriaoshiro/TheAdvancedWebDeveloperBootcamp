923553f3fc106f3657d6beea64c05c5378b73df0
valeriaoshiro <valeriaoshiro19@gmail.com>
2018-03-29 17:28:32 +0000

    Add stateless component

--- update-complex-state/src/App.js
+++ update-complex-state/src/App.js
@@ -2,19 +2,18 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import './App.css';
 
-class InstructorItem extends Component {
-  static propTypes = {
-    name: PropTypes.string,
-    hobbies: PropTypes.arrayOf(PropTypes.string)
-  }
-  render(){
-    return (
-      <li key={this.props.key}>
-        <h3>{this.props.name}</h3>
-        <h4>Hobbies: {this.props.hobbies.join(", ")}</h4>
-      </li>
-    );
-  }
+const InstructorItem = props => {
+  return (
+    <li key={props.key}>
+      <h3>{props.name}</h3>
+      <h4>Hobbies: {props.hobbies.join(", ")}</h4>
+    </li>
+  );
+}
+
+InstructorItem.propTypes = {
+  name: PropTypes.string,
+  hobbies: PropTypes.arrayOf(PropTypes.string)
 }
 
 class App extends Component {
