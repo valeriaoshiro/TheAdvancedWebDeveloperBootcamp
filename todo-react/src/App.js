7c45c7ee8f5bf441f206a01797fd0fde04b260eb
valeriaoshiro <valeriaoshiro19@gmail.com>
2018-03-30 22:33:15 +0000

    Created react form

--- todo-react/src/App.js
+++ todo-react/src/App.js
@@ -1,18 +1,44 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      data: [],
+      inputText: ''
+    }
+    this.handleClick = this.handleClick.bind(this);
+  }
+  handleClick(e){
+    e.preventDefault();
+    const data = [...this.state.data,
+                  this.state.inputText];
+	 this.setState({data, inputText: ''});
+  }
   render() {
+    const todos = this.state.data.map((t, i) => {
+      return (
+        <li key={i}>{t}</li>  
+      )
+    });
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+          <h1>Simple Todo App</h1>
+          <form onSubmit={this.handleClick}>
+    	      <input
+    		      type='text'
+    		      name='inputText'
+    		      value={this.state.inputText}
+    		      onChange={e => {
+    			      this.setState({[e.target.name]: e.target.value})
+              }}
+            />
+            <button type='submit'>SAVE</button>
+          </form>
+          <ol>
+            {todos}
+          </ol>
       </div>
     );
   }
