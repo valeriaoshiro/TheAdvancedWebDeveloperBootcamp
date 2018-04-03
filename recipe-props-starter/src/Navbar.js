71e88669ebbe128e55c4cf42a908dc9a5d88120f
valeriaoshiro <valeriaoshiro19@gmail.com>
2018-04-03 17:34:09 +0000

    Add add form. It is able to show and disapear

--- recipe-props-starter/src/Navbar.js
+++ recipe-props-starter/src/Navbar.js
@@ -1,13 +1,20 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import './Navbar.css'
 
 class Navbar extends Component {
+    static defaultProps = {
+        onNewRecipe(){}
+    }
+    static propTypes = {
+        onNewRecipe: PropTypes.func
+    }
     render(){
         return (
             <div className='nav'>
                 <div>Recipe App</div>
                 <ul>
-                    <li>New Recipe</li>
+                    <li><a onClick={this.props.onNewRecipe}>New Recipe</a></li>
                     <li>Home</li>
                     <li>About</li>
                     <li>Contact Us</li>
--- recipe-props-starter/src/RecipeApp.js
+++ recipe-props-starter/src/RecipeApp.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import RecipeList from './RecipeList';
+import RecipeInput from './RecipeInput';
 import Navbar from './Navbar';
 import './RecipeApp.css';
 
@@ -10,34 +11,56 @@ class RecipeApp extends Component {
     this.state = {
       recipes: [
             {
-                id: 1,
+                id: 0,
                 title: "Spaghetti",
                 instructions: "Open jar of Spaghetti sauce.  Bring to simmer.  Boil water.  Cook pasta until done.  Combine pasta and sauce",
                 ingredients: ["pasta", "8 cups water", "1 box spaghetti"],
                 img: "spaghetti.jpg"
             },
             {
-                id: 2,
+                id: 1,
                 title: "Milkshake",
                 instructions: "Combine ice cream and milk.  Blend until creamy",
                 ingredients: ["2 Scoops Ice cream", "8 ounces milk"],
                 img: "spaghetti.jpg"
             },
             {
-                id: 3,
+                id: 2,
                 title: "Avocado Toast",
                 instructions: "Toast bread.  Slice avocado and spread on bread.  Add salt, oil, and pepper to taste.",
                 ingredients: ["2 slices of bread", "1 avocado", "1 tablespoon olive oil", "1 pinch of salt", "pepper"],
                 img: "spaghetti.jpg"
             }
         ],
-        nextRecipeId: 3
+        nextRecipeId: 3,
+        showForm: false
     }
+    this.handleSave = this.handleSave.bind(this);
   }
+  
+  handleSave(recipe){
+    this.setState((prevState, props) => {
+      const newRecipe = {...recipe, id: this.state.nextRecipeId};
+      return {
+        nextRecipeId: prevState.nextRecipeId + 1,
+        recipes: [...this.state.recipes, newRecipe],
+        showForm: false
+      }
+    });
+  }
+
+  
   render() {
+    const {showForm} = this.state;
     return (
       <div className="App">
-        <Navbar />
+        <Navbar onNewRecipe={() => this.setState({showForm: true})}/>
+        {showForm ? 
+          <RecipeInput 
+            onSave={this.handleSave} 
+            onClose={() => this.setState({showForm: false})}
+          /> 
+        : null }
         <RecipeList recipes={this.state.recipes}/> 
       </div>
     );
