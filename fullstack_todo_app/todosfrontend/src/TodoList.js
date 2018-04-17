9e32617407c9b02bee5393a33544e5c94e9b5712
valeriaoshiro <valeriaoshiro19@gmail.com>
2018-04-17 22:22:11 +0000

    Create TodoList component

---
+++ fullstack_todo_app/todosfrontend/src/TodoList.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class TodoList extends Component {
+  render() {
+    return (
+      <h1>This is the todolist file</h1>
+    );
+  }
+}
+
+export default TodoList;
