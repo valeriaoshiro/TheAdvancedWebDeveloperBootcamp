0e8b81a316e6f0f4ec775c812c85187115c9066f
valeriaoshiro <valeriaoshiro19@gmail.com>
2018-04-18 17:33:56 +0000

    Able to fetch information from the backend

--- fullstack_todo_app/todosfrontend/src/TodoList.js
+++ fullstack_todo_app/todosfrontend/src/TodoList.js
@@ -1,9 +1,44 @@
 import React, { Component } from 'react';
 
 class TodoList extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      todos: []  
+    };
+  }
+  
+  componentDidMount(){
+    const countriesURL = '/api/todos';
+    fetch(countriesURL)
+    .then(resp => {
+      if(!resp.ok){
+        if(resp.status >= 400 && resp.status < 500){
+          return resp.json().then(data => {
+            let err = {errorMessage: data.message};
+            throw err;
+          })
+        } else {
+          let err = {errorMessage: 'Please try again later'};
+          throw err;
+        }
+      }
+      return resp.json();
+    })
+    .then(todos => {
+        this.setState({todos})
+    })
+  }
+
   render() {
+    const todos = this.state.todos.map(todo => {
+      return <p>{todo.name}</p>
+    })
     return (
-      <h1>This is the todolist file</h1>
+      <div>
+        <h1>This is the todolist file</h1>
+        {todos}
+      </div>
     );
   }
 }
